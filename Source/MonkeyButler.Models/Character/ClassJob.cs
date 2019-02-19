979aefa06925a7875977ae8891b22ebb5b5e9cdc
Foshkey <foshkey@gmail.com>
2019-02-19 10:12:58 -0600

    Added more models representing the xiv api.

---
+++ Source/MonkeyButler.Models/Character/ClassJob.cs
@@ -0,0 +1,38 @@
+﻿namespace MonkeyButler.Models.Character
+{
+    /// <summary>
+    /// A model representing a character's class/job.
+    /// </summary>
+    public class ClassJob
+    {
+        /// <summary>
+        /// The class Id.
+        /// </summary>
+        public int ClassId { get; set; }
+
+        /// <summary>
+        /// The current experience invested in the current level.
+        /// </summary>
+        public int ExpLevel { get; set; }
+
+        /// <summary>
+        /// The max experience for the current level.
+        /// </summary>
+        public int ExpLevelMax { get; set; }
+
+        /// <summary>
+        /// The amount of experience to complete the current level.
+        /// </summary>
+        public int ExpLevelToGo { get; set; }
+
+        /// <summary>
+        /// The job Id.
+        /// </summary>
+        public int JobId { get; set; }
+
+        /// <summary>
+        /// The current level of the class/job.
+        /// </summary>
+        public int Level { get; set; }
+    }
+}
