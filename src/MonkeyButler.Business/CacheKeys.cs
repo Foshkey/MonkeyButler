70071396b0d2e68b852fc8d1f220791e94e227b4
Foshkey <foshkey@gmail.com>
2020-06-20 14:04:39 -0500

    Created events cache key

--- src/MonkeyButler.Business/CacheKeys.cs
+++ src/MonkeyButler.Business/CacheKeys.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using MonkeyButler.Business.Models.Events;
 using MonkeyButler.Business.Options;
 using MonkeyButler.Data.Cache;
 
@@ -7,6 +8,7 @@ namespace MonkeyButler.Business
     internal static class CacheKeys
     {
         public static readonly string GuildOptions = "MonkeyButler:GuildOptions";
+        public static readonly string Events = "MonkeyButler:Events";
     }
 
     internal static class CacheExtensions
@@ -16,5 +18,11 @@ namespace MonkeyButler.Business
 
         public static async Task SetGuildOptions(this IAccessor accessor, GuildOptionsDictionary guildOptions)
             => await accessor.Write(CacheKeys.GuildOptions, guildOptions);
+
+        public static async Task<Event?> GetEvent(this IAccessor accessor, long eventId)
+            => await accessor.Read<Event>($"{CacheKeys.Events}:{eventId}");
+
+        public static async Task SetEvent(this IAccessor accessor, Event eventInfo)
+            => await accessor.Write($"{CacheKeys.Events}:{eventInfo.Id}", eventInfo);
     }
 }
---
+++ src/MonkeyButler.Business/Models/Events/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonkeyButler.Business.Models.Events
+{
+    /// <summary>
+    /// A model representing an FC event.
+    /// </summary>
+    public class Event
+    {
+        /// <summary>
+        /// The Id of the event.
+        /// </summary>
+        public long Id { get; set; }
+    }
+}
