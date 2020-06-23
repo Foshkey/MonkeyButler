d2005c9dc1e01cf14f12047088b4815a02177734
Foshkey <foshkey@gmail.com>
2020-06-23 13:53:57 -0500

    Parsing engine work

--- src/MonkeyButler.Business/Engines/EventParsingEngine.cs
+++ src/MonkeyButler.Business/Engines/EventParsingEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using MonkeyButler.Business.Models.Events;
 
 namespace MonkeyButler.Business.Engines
@@ -8,6 +9,8 @@ namespace MonkeyButler.Business.Engines
     {
         private const string _timeKeyWord = "at";
         private const string _dateKeyWord = "on";
+        private const string _todayKeyWord = "today";
+        private const string _tomorrowKeyWord = "tomorrow";
 
         private readonly Dictionary<string, DayOfWeek> _dayOfWeekMap = new Dictionary<string, DayOfWeek>(StringComparer.OrdinalIgnoreCase)
         {
@@ -39,20 +42,140 @@ namespace MonkeyButler.Business.Engines
         // Needed to break out "now" for unit testing.
         public Event Parse(string query, TimeSpan tzOffset, DateTimeOffset now)
         {
-            var newEvent = new Event();
-            var words = query.Split(' ');
-
-            var timeKeyIndex = Array.IndexOf(words, _timeKeyWord);
-            var dateKeyIndex = Array.IndexOf(words, _dateKeyWord);
-
-            if (timeKeyIndex < 0 && dateKeyIndex < 0)
+            var newEvent = new Event()
             {
-                throw new InvalidOperationException($"Query '{query}' not formed properly. Could not find key words 'at' or 'on'.");
-            }
+                CreationDateTime = now
+            };
 
+            query = query.Trim(' ', '.', '!', '?');
+            var words = query.Split(' ');
+            var wordsList = words.ToList();
+            now = now.ToOffset(tzOffset);
 
+            var timeKeyIndex = wordsList.FindIndex(x => x.Equals(_timeKeyWord, StringComparison.OrdinalIgnoreCase));
+            var dateKeyIndex = wordsList.FindIndex(x => x.Equals(_dateKeyWord, StringComparison.OrdinalIgnoreCase));
+
+            var titleEndIndex = GetEndIndex(timeKeyIndex, dateKeyIndex);
+            newEvent.Title = string.Join(' ', words[..titleEndIndex]);
+
+            var timeEndIndex = timeKeyIndex < dateKeyIndex
+                ? dateKeyIndex
+                : words.Length;
+
+            var dateEndIndex = dateKeyIndex < timeKeyIndex
+                ? timeKeyIndex
+                : words.Length;
+
+            var timeStr = string.Join(' ', words[(timeKeyIndex + 1)..timeEndIndex]);
+            var dateStr = string.Join(' ', words[(dateKeyIndex + 1)..dateEndIndex]);
+
+            var time = DateTimeOffset.TryParse(timeStr, out var timeParsed)
+                ? timeParsed
+                : FindTimeElsewhere(words);
+
+            var date = DateTimeOffset.TryParse(dateStr, out var dateParsed)
+                ? dateParsed
+                : FindDateElsewhere(words, dateStr, now);
+
+            newEvent.EventDateTime = Combine(time, date, now, tzOffset)
+                ?? throw new InvalidOperationException($"Could not determine event date/time with query '{query}'.");
 
             return newEvent;
+        }
+
+        private int GetEndIndex(int i1, int i2)
+        {
+            if (i1 < 0)
+            {
+                return i2;
+            }
+
+            if (i2 < 0)
+            {
+                return i1;
+            }
+
+            return i1 < i2 ? i1 : i2;
+        }
+
+        private DateTimeOffset? FindTimeElsewhere(string[] words)
+        {
+            // Strategy for this is try parsing each word, and then sequential pairing of words.
+            // Both from right to left, as title is last in priority of parsing.
+
+            foreach (var word in words.Reverse())
+            {
+                if (DateTimeOffset.TryParse(word, out var time))
+                {
+                    return time;
+                }
+            }
+
+            for (var i = words.Length - 2; i >= 0; i--)
+            {
+                var pair = string.Join(' ', words[i], words[i + 1]);
+                if (DateTimeOffset.TryParse(pair, out var time))
+                {
+                    return time;
+                }
+            }
+
+            return null;
+        }
+
+        private DateTimeOffset? FindDateElsewhere(string[] words, string dateStr, DateTimeOffset now)
+        {
+            // Check day of week
+            if (_dayOfWeekMap.ContainsKey(dateStr))
+            {
+                return GetNextDay(_dayOfWeekMap[dateStr], now);
+            }
+
+            // Day of week in reverse order
+            foreach (var word in words.Reverse())
+            {
+                if (_dayOfWeekMap.ContainsKey(word))
+                {
+                    return GetNextDay(_dayOfWeekMap[word], now);
+                }
+            }
+
+            // Tomorrow in reverse order
+            foreach (var word in words.Reverse())
+            {
+                if (string.Equals(_tomorrowKeyWord, word, StringComparison.OrdinalIgnoreCase))
+                {
+                    return now.AddDays(1);
+                }
+            }
+
+            return now;
+        }
+
+        private DateTimeOffset GetNextDay(DayOfWeek dayOfWeek, DateTimeOffset now)
+        {
+            var diff = dayOfWeek - now.DayOfWeek;
+            if (diff < 0) diff += 7;
+
+            return now.AddDays(diff);
+        }
+
+        private DateTimeOffset? Combine(DateTimeOffset? time, DateTimeOffset? date, DateTimeOffset now, TimeSpan offset)
+        {
+            if (time is null || date is null)
+            {
+                return null;
+            }
+
+            return new DateTimeOffset(
+                year: date.Value.Year,
+                month: date.Value.Month,
+                day: date.Value.Day,
+                hour: time.Value.Hour,
+                minute: time.Value.Minute,
+                second: time.Value.Second,
+                offset: offset
+            );
         }
     }
 
--- tests/MonkeyButler.Business.Tests/Engine/EventParsingEngineTests.cs
+++ tests/MonkeyButler.Business.Tests/Engine/EventParsingEngineTests.cs
@@ -10,9 +10,27 @@ namespace MonkeyButler.Business.Tests.Engine
     {
         // Now = Saturday, June 20, Noon EDT
         private static readonly TimeSpan _tzOffsetInput = TimeSpan.FromHours(-4);
-        private static readonly DateTimeOffset _nowInput = new DateTimeOffset(2020, 6, 20, 16, 0, 0, TimeSpan.Zero);
+        private static DateTimeOffset _nowInput = new DateTimeOffset(2020, 6, 20, 16, 0, 0, TimeSpan.Zero);
 
         private Event Parse(string query, TimeSpan tzOffset) => new SUT().Parse(query, tzOffset, _nowInput);
+
+        [Fact]
+        public void NextYearWorks()
+        {
+            _nowInput = new DateTimeOffset(2020, 12, 31, 16, 0, 0, TimeSpan.Zero);
+            var query = "Next year test at 7am.";
+            var expectedEvent = new Event()
+            {
+                Title = "Next year test",
+                EventDateTime = _nowInput.AddHours(24 - 5).ToOffset(_tzOffsetInput)
+            };
+
+            var result = Parse(query, _tzOffsetInput);
+
+            Assert.Equal(expectedEvent.Title, result.Title);
+            Assert.Equal(_nowInput, result.CreationDateTime);
+            Assert.Equal(expectedEvent.EventDateTime, result.EventDateTime);
+        }
 
         [Theory]
         [MemberData(nameof(TestData))]
@@ -21,7 +39,8 @@ namespace MonkeyButler.Business.Tests.Engine
             var result = Parse(query, tzOffset);
 
             Assert.Equal(expectedEvent.Title, result.Title);
-            Assert.True(expectedEvent.EventDateTime.Equals(result.EventDateTime));
+            Assert.Equal(_nowInput, result.CreationDateTime);
+            Assert.Equal(expectedEvent.EventDateTime, result.EventDateTime);
         }
 
         private static IEnumerable<object[]> TestData()
@@ -39,12 +58,23 @@ namespace MonkeyButler.Business.Tests.Engine
 
             yield return new object[]
             {
-                "Dumb stuff at 7am.",
+                "Bunch of periods at 9pm......",
                 _tzOffsetInput,
                 new Event()
                 {
-                    Title = "Dumb stuff",
-                    EventDateTime = _nowInput.AddHours(24 - 5).ToOffset(_tzOffsetInput)
+                    Title = "Bunch of periods",
+                    EventDateTime = _nowInput.AddHours(9).ToOffset(_tzOffsetInput)
+                }
+            };
+
+            yield return new object[]
+            {
+                "Today's dumb stuff at 7:30 am tomorrow.",
+                _tzOffsetInput,
+                new Event()
+                {
+                    Title = "Today's dumb stuff",
+                    EventDateTime = _nowInput.AddHours(24 - 4.5).ToOffset(_tzOffsetInput)
                 }
             };
 
@@ -61,12 +91,23 @@ namespace MonkeyButler.Business.Tests.Engine
 
             yield return new object[]
             {
+                "Tomorrow is another day at 5pm Monday.",
+                _tzOffsetInput,
+                new Event()
+                {
+                    Title = "Tomorrow is another day",
+                    EventDateTime = _nowInput.AddHours(24 * 2 + 5).ToOffset(_tzOffsetInput)
+                }
+            };
+
+            yield return new object[]
+            {
                 "Monday fun times on Monday at 14:30",
                 _tzOffsetInput,
                 new Event()
                 {
                     Title = "Monday fun times",
-                    EventDateTime = _nowInput.AddHours(24 + 24 + 2.5).ToOffset(_tzOffsetInput)
+                    EventDateTime = _nowInput.AddHours(24 * 2 + 2.5).ToOffset(_tzOffsetInput)
                 }
             };
 
@@ -78,6 +119,72 @@ namespace MonkeyButler.Business.Tests.Engine
                 {
                     Title = "Saturday morning beats",
                     EventDateTime = _nowInput.AddHours(24 * 7 - 3).ToOffset(_tzOffsetInput)
+                }
+            };
+
+            yield return new object[]
+            {
+                "Weird date parsing on Thursday 2pm",
+                _tzOffsetInput,
+                new Event()
+                {
+                    Title = "Weird date parsing",
+                    EventDateTime = _nowInput.AddHours(24 * 5 + 2).ToOffset(_tzOffsetInput)
+                }
+            };
+
+            yield return new object[]
+            {
+                "Specific date parsing on June 22 at 4pm",
+                _tzOffsetInput,
+                new Event()
+                {
+                    Title = "Specific date parsing",
+                    EventDateTime = _nowInput.AddHours(24 * 2 + 4).ToOffset(_tzOffsetInput)
+                }
+            };
+
+            yield return new object[]
+            {
+                "Maybe I can get this to work tomorrow at 8pm",
+                _tzOffsetInput,
+                new Event()
+                {
+                    Title = "Maybe I can get this to work",
+                    EventDateTime = _nowInput.AddHours(24 + 8).ToOffset(_tzOffsetInput)
+                }
+            };
+
+            yield return new object[]
+            {
+                "Super special case thu 1 pm",
+                _tzOffsetInput,
+                new Event()
+                {
+                    Title = "Super special case",
+                    EventDateTime = _nowInput.AddHours(24 * 5 + 1).ToOffset(_tzOffsetInput)
+                }
+            };
+
+            yield return new object[]
+            {
+                "Rebelling against Tuesday on Monday at 6:15pm",
+                _tzOffsetInput,
+                new Event()
+                {
+                    Title = "Rebelling against Tuesday",
+                    EventDateTime = _nowInput.AddHours(24 * 2 + 6.25).ToOffset(_tzOffsetInput)
+                }
+            };
+
+            yield return new object[]
+            {
+                "Crazy offset at 8am tomorrow",
+                TimeSpan.FromHours(7), // now = June 21 1am local
+                new Event()
+                {
+                    Title = "Crazy offset",
+                    EventDateTime = _nowInput.AddHours(24 + 7).ToOffset(TimeSpan.FromHours(7))
                 }
             };
         }
