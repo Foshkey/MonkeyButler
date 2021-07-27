1e31ac9dd41abfca37ac010dc073fe95d7ca7338
Joshua Nelson <foshkey@gmail.com>
2021-07-27 11:51:24 -0500

    Fixed export

--- src/MonkeyButler.Data.Storage/ImportExportAccessor.cs
+++ src/MonkeyButler.Data.Storage/ImportExportAccessor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using MonkeyButler.Abstractions.Data.Storage;
 using MonkeyButler.Abstractions.Data.Storage.Models.ImportExport;
@@ -12,30 +13,32 @@ namespace MonkeyButler.Data.Storage
     internal class ImportExportAccessor : IImportExportAccessor
     {
         private readonly IConnectionMultiplexer _connectionMultiplexer;
+        private readonly IDistributedCache _distributedCache;
         private readonly ILogger<ImportExportAccessor> _logger;
 
-        public ImportExportAccessor(IConnectionMultiplexer connectionMultiplexer, ILogger<ImportExportAccessor> logger)
+        public ImportExportAccessor(IConnectionMultiplexer connectionMultiplexer, IDistributedCache distributedCache, ILogger<ImportExportAccessor> logger)
         {
             _connectionMultiplexer = connectionMultiplexer ?? throw new ArgumentNullException(nameof(connectionMultiplexer));
+            _distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
-
-        private IDatabase _database => _connectionMultiplexer.GetDatabase();
-        private IServer _server => _connectionMultiplexer.GetServer(_connectionMultiplexer.GetEndPoints().First());
 
         public async Task<ExportData> ExportAll()
         {
             _logger.LogDebug("Exporting all data from Redis.");
 
-            var keys = await Task.Run(() => _server.Keys());
+            var server = _connectionMultiplexer.GetServer(_connectionMultiplexer.GetEndPoints().First());
+            var keys = await Task.Run(() => server.Keys());
             var tasks = new List<Task>();
-            var database = _database;
             var data = new ExportData();
 
             foreach (var key in keys)
             {
-                tasks.Add(database.StringGetAsync(key)
-                    .ContinueWith(async value => data.Export.Add(key, (await value).ToString())));
+                tasks.Add(
+                    _distributedCache.GetStringAsync(key).ContinueWith(
+                        task => data.Export.Add(key, task.Result)
+                    )
+                );
             }
 
             await Task.WhenAll(tasks);
